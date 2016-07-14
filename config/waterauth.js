917d220b1d00d1c8aa514e5ceacd57a5bf84dc45
noriah <vix@noriah.dev>
2016-07-14 01:15:16 -0500

    Updating with code climate stuffs

---
+++ config/waterauth.js
@@ -0,0 +1,12 @@
+module.exports.waterauth = {
+  bcrypt: {
+    rounds: 8
+  },
+
+  adminFirstName: process.env.ADMIN_FNAME || 'Admin',
+  adminLastName: process.env.ADMIN_LNAME || 'McAdminFace',
+  adminEmail: process.env.ADMIN_EMAIL || 'admin@example.com',
+  adminUsername: process.env.ADMIN_USERNAME || 'admin',
+  adminPassword: process.env.ADMIN_PASSWORD || 'admin1234'
+
+}
