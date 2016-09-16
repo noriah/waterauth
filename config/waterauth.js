fa6cc6ac0b88116d64e068edf330b9c4aa3f2604
Austin Reuland <amreuland@gmail.com>
2016-09-16 14:31:04 -0500

    Update policies and fix not being able to populate objects

--- config/waterauth.js
+++ config/waterauth.js
@@ -9,6 +9,8 @@ module.exports.waterauth = {
   adminLastName: process.env.ADMIN_LNAME || 'McAdminFace',
   adminEmail: process.env.ADMIN_EMAIL || 'admin@example.com',
   adminUsername: process.env.ADMIN_USERNAME || 'admin',
-  adminPassword: process.env.ADMIN_PASSWORD || 'admin1234'
+  adminPassword: process.env.ADMIN_PASSWORD || 'admin1234',
+
+  trackGetRequests: false
 
 }
